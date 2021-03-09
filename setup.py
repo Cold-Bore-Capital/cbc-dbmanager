da89d7e522d97c1bd0e4f0b93a64ce1ef7678b27
Craig Lathrop <clathrop@coldborecapital.com>
2021-03-08 18:22:47 -0700

    please work

--- setup.py
+++ setup.py
@@ -1,32 +1,37 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-VERSION = '0.0.1'
-DESCRIPTION = 'A python package to assist with connecting to Redshift via an SSH tunnel.'
-LONG_DESCRIPTION = 'A python package to assist with connecting to Redshift via an SSH tunnel.'
+if __name__ == "__main__":
+    setup()
 
-# Setting up
-setup(
-    # Name of the folder containing module.
-    name="cbcdb",
-    version=VERSION,
-    author="CBC IT Group",
-    author_email="it-group@coldborecapital.com",
-    description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
-    packages=find_packages(),
-    url='https://github.com/Cold-Bore-Capital/cbc-dbmanager',
-    install_requires=[
-        'psycopg2-binary>=2.8',
-        'sshtunnel>=0.4.0',
-        'pandas>=1.2'
-    ],
-
-    keywords=['python', 'redshift', 'ssh'],
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English"
-    ]
-)
+# from setuptools import setup, find_packages
+#
+# VERSION = '0.0.1'
+# DESCRIPTION = 'A python package to assist with connecting to Redshift via an SSH tunnel.'
+# LONG_DESCRIPTION = 'A python package to assist with connecting to Redshift via an SSH tunnel.'
+#
+# # Setting up
+# setup(
+#     # Name of the folder containing module.
+#     name="cbcdb",
+#     version=VERSION,
+#     author="CBC IT Group",
+#     author_email="it-group@coldborecapital.com",
+#     description=DESCRIPTION,
+#     long_description=LONG_DESCRIPTION,
+#     packages=find_packages(),
+#     url='https://github.com/Cold-Bore-Capital/cbc-dbmanager',
+#     install_requires=[
+#         'psycopg2-binary>=2.8',
+#         'sshtunnel>=0.4.0',
+#         'pandas>=1.2'
+#     ],
+#
+#     keywords=['python', 'redshift', 'ssh'],
+#     classifiers=[
+#         "Development Status :: 4 - Beta",
+#         "Intended Audience :: Developers",
+#         "Programming Language :: Python :: 3.8",
+#         "License :: OSI Approved :: MIT License",
+#         "Natural Language :: English"
+#     ]
+# )
