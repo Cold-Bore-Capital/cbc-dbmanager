9a6ac93a7b84bbda994af0e11378254220619a1f
Craig Lathrop <clathrop@coldborecapital.com>
2021-03-15 17:44:07 -0700

    Going to empty find_packages bit.

--- setup.py
+++ setup.py
@@ -3,7 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name='cbcdb',
     # packages=['cbcdb', 'cbcdb.main']
-    packages=find_packages(
-        where='cbcdb'
-    ),
+    packages=find_packages(),
 )
